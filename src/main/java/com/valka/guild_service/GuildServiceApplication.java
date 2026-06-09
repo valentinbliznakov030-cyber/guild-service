60d696ad8dcb5f127c244854e10d5189129f1c7c
valentinbliznakov030 <valentinbliznakov030@gmail.com>
2026-06-09 13:00:09 +0300

    init project

---
+++ src/main/java/com/valka/guild_service/GuildServiceApplication.java
@@ -0,0 +1,13 @@
+package com.valka.guild_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valka/guild_service/GuildServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valka.guild_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
